0f63b5df08d9d7e877f3de68d52323f6306eabb7
kristopher tate <kristopher@connectfree.jp>
2012-09-03 04:08:29 +0900

    MagLev: updated example server-http-simple to use buffers;

--- examples/lev/server-http-simple.lua
+++ examples/lev/server-http-simple.lua
@@ -1,5 +1,7 @@
 local levbase = require('levbase')
 
+local RES_BUFFER = Buffer:new("HTTP/1.0 200 OK\r\nConnection: Close\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\nHello World\n\r\n")
+
 local server = levbase.tcp.new()
 server:bind("0.0.0.0", 8080)
 server:listen(function(s, err)
@@ -10,7 +12,7 @@ server:listen(function(s, err)
   client:read_start(function(c, nread, buf)
     -- we do not have to worry about closing our client here...
     -- that is taken care of automatically! for events, register with client:on_close()
-    c:write("HTTP/1.0 200 OK\r\nConnection: Close\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\nHello World\n\r\n")
+    c:write( RES_BUFFER )
     c:close()
   end)
 end)
