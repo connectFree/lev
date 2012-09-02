1f84efcfebdd765e776abaf4711246663061dfb3
kristopher tate <kristopher@connectfree.jp>
2012-09-03 04:34:13 +0900

    MagLev: server-http-simple: define one read function instead of creating multiple callbacks;

--- examples/lev/server-http-simple.lua
+++ examples/lev/server-http-simple.lua
@@ -2,17 +2,16 @@ local levbase = require('levbase')
 
 local RES_BUFFER = Buffer:new("HTTP/1.0 200 OK\r\nConnection: Close\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\nHello World\n\r\n")
 
+local client__on_read = function(c, nread, buf)
+  -- we do not have to worry about closing our client here...
+  -- that is taken care of automatically! for events, register with client:on_close()
+  c:write( RES_BUFFER )
+  c:close()
+end
+
 local server = levbase.tcp.new()
 server:bind("0.0.0.0", 8080)
 server:listen(function(s, err)
   local client = s:accept()
---  client:on_close(function(c)
---    client = nil
---  end)
-  client:read_start(function(c, nread, buf)
-    -- we do not have to worry about closing our client here...
-    -- that is taken care of automatically! for events, register with client:on_close()
-    c:write( RES_BUFFER )
-    c:close()
-  end)
+  client:read_start(client__on_read)
 end)
