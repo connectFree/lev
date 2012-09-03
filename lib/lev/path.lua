195e3b11f420b7aa97101b166f696e82eef33927
Hiroaki Nakamura <hnakamur@gmail.com>
2012-09-04 03:35:05 +0900

    (WI) partially implement path as plain module, not as an Object.

--- lib/lev/path.lua
+++ lib/lev/path.lua
@@ -1,6 +1,6 @@
 --[[
 
-Copyright 2012 The Luvit Authors. All Rights Reserved.
+Copyright 2012 The lev Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,44 +16,47 @@ limitations under the License.
 
 --]]
 
-local Object = require('core').Object
-local os = require('os_binding')
+local lev = require('lev')
+local string = require('string')
 local table = require('table')
 
 local path = {}
 
-local Path = Object:extend()
-path.Path = Path
-
-function Path:initialize(root, sep)
-  self.root = root
-  self.sep = sep
+if lev.os_type() == 'win32' then
+  path.sep = '\\'
+else
+  path.sep = '/'
 end
+
+local SEP_BYTE = string.byte(path.sep)
+
+--[[
+TODO: implement
 
 -- Split a filename into [root, dir, basename], unix version
 -- 'root' is just a slash, or nothing.
 function Path:_splitPath(filename)
   local root, dir, basename
-  local i, j = filename:find("[^" .. self.sep .. "]*$")
+  local i, j = filename:find('[^' .. self.sep .. ']*$')
   if filename:sub(1, 1) == self.sep then
     root = self.root
     dir = filename:sub(2, i - 1)
   else
-    root = ""
+    root = ''
     dir = filename:sub(1, i - 1)
   end
   local basename = filename:sub(i, j)
   return root, dir, basename, ext
 end
 
--- Modifies an array of path parts in place by interpreting "." and ".." segments
+-- Modifies an array of path parts in place by interpreting '.' and '..' segments
 function Path:_normalizeArray(parts)
   local skip = 0
   for i = #parts, 1, -1 do
     local part = parts[i]
-    if part == "." then
+    if part == '.' then
       table.remove(parts, i)
-    elseif part == ".." then
+    elseif part == '..' then
       table.remove(parts, i)
       skip = skip + 1
     elseif skip > 0 then
@@ -63,12 +66,22 @@ function Path:_normalizeArray(parts)
   end
 end
 
-function Path:normalize(filepath)
+function Path:resolve(root, filepath)
+  if filepath:sub(1, self.root:len()) == self.root then
+    return self:normalize(filepath)
+  end
+  return self:join(root, filepath)
+end
+--]]
+
+function path.normalize(filepath)
+--[[
+TODO: implement
   local is_absolute = filepath:sub(1, 1) == self.sep
   local trailing_slash = filepath:sub(#filepath) == self.sep
 
   local parts = {}
-  for part in filepath:gmatch("[^" .. self.sep .. "]+") do
+  for part in filepath:gmatch('[^' .. self.sep .. ']+') do
     parts[#parts + 1] = part
   end
   self:_normalizeArray(parts)
@@ -78,7 +91,7 @@ function Path:normalize(filepath)
     if is_absolute then
       return self.sep
     end
-    return "."
+    return '.'
   end
   if trailing_slash then
     filepath = filepath .. self.sep
@@ -86,65 +99,51 @@ function Path:normalize(filepath)
   if is_absolute then
     filepath = self.sep .. filepath
   end
+--]]
   return filepath
 end
 
-function Path:join(...)
-  return table.concat({...}, self.sep)
+function path.join(...)
+  return path.normalize(table.concat({...}, path.sep))
 end
 
-function Path:resolve(root, filepath)
-  if filepath:sub(1, self.root:len()) == self.root then
-    return self:normalize(filepath)
-  end
-  return self:join(root, filepath)
-end
-
-function Path:dirname(filepath)
-  if filepath:sub(filepath:len()) == self.sep then
-    filepath = filepath:sub(1, -2)
-  end
-
-  local root, dir = self:_splitPath(filepath)
-
-  if #dir > 0 then
-    dir = dir:sub(1, #dir - 1)
-    return root .. dir
-  end
-  if #root > 0 then
-    return root
-  end
-  return "."
-
-end
-
-function Path:basename(filepath, expected_ext)
-  return filepath:match("[^" .. self.sep .. "]+$") or ""
-end
-
-function Path:extname(filepath)
-  return filepath:match(".[^.]+$") or ""
-end
-
-path.nt = Path:new("c:", "\\")
-path.posix = Path:new("/", "/")
-
-local function setup_meta(ospath)
-  setmetatable(path, {__index = function(table, key)
-      if type(ospath[key]) == 'function' then
-        return function(...) return ospath[key](ospath, ...) end
-      else
-        return ospath[key]
-      end
+local function rfind_byte(haystack, needle_byte)
+  for i = #haystack, 1, -1 do
+    if string.byte(haystack, i) == needle_byte then
+      return i
     end
-  })
+  end
+  return nil
 end
 
+function path.dirname(filepath)
+  local i = rfind_byte(filepath, SEP_BYTE)
+  return i and string.sub(filepath, 1, i - 1) or filepath
+end
 
-if os.type() == "win32" then
-  setup_meta(path.nt)
-else
-  setup_meta(path.posix)
+-- TODO: move this to string util module, and create a native function.
+local function ends_with(str, suffix)
+  return string.sub(str, #str - #suffix + 1) == suffix
+end
+
+function path.basename(filepath, ext)
+  local i = rfind_byte(filepath, SEP_BYTE)
+  if i then
+    if ext and ends_with(filepath, ext) then
+      return string.sub(filepath, i + 1, #filepath - #ext)
+    else
+      return string.sub(filepath, i + 1)
+    end
+  else
+    return filepath
+  end
+end
+
+local DOT_BYTE = string.byte('.')
+
+function path.extname(filepath)
+  local i = rfind_byte(filepath, DOT_BYTE)
+  return i and string.sub(filepath, i) or ''
 end
 
 return path
